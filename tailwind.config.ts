76e6a9cb1e0d11a034e4507c3bfad20b075eeaa5
Thalessj <estavamthalles@gmail.com>
2024-01-15 01:00:14 -0300

    frs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,49 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: { 
+      'xxsm': '300px',
+      // => @media (min-width: 640px) { ... }
+
+      'xsm': '320px',
+      // => @media (min-width: 640px) { ... }
+      
+      '2sm': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      '3sm': '425px',
+      // => @media (min-width: 640px) { ... }
+      
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '3xl': '1720px',
+      // => @media (min-width: 1720px) { ... }
+
+      '4xl': '1920px',
+      // => @media (min-width: 1920px) { ... }
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'gold1': '#f0c15f',
+        'gold2': '#f6d078',
+        'light': '#d6d6d6',
+        'dark1': '#181818',
+        'dark2': '#333333',
+        'dark3': '#2F2F2F',
+        'teste': '#565656',
       },
     },
   },
